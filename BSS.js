11a29212eddcf6475b2327c9108fec6a19787838
Boris Karbanovich <karb@scand.com>
2014-10-27 20:32:49 +0300

    BSS - added verification of window.BSS-existance, changed names of variables in BSS main closure (added one more '_' to each one).

--- BSS.js
+++ BSS.js
@@ -1,208 +1,224 @@
-﻿(function (_win, _coreNamespace, _coreModules) {
-	var _doc = _win.document,
-		_head = _doc.head,
-		_bss = {},
-		_bssModules = (function () {
-			var _definedModules = {},
-				_registeredNamespaces = {},
-				_callbacks = {},
-				_getModuleNameWithNamespace = function (moduleFullName) {
-					var modulePathChain = moduleFullName.split("."),
-						nsName;
-					for (nsName in _registeredNamespaces) {
-						if (nsName === modulePathChain[0]) {
-							modulePathChain[0] = _registeredNamespaces[nsName];
-							moduleFullName = modulePathChain.join(".");
-							break;
-						}
+﻿(function (__win, __coreNamespace, __coreModules) {
+	var __doc, __head, __bss, __bssModules, __bssModules_preloadFake;
+
+	if ((__win || window).hasOwnProperty("BSS")) {
+		return;
+	}
+
+	__win = __win || window;
+	__coreNamespace = __coreNamespace || "";
+	__coreModules = __coreModules || [];
+
+	__doc = __win.document;
+	__head = __doc.head;
+	__bss = {};
+	__bssModules = (function () {
+		var _definedModules = {},
+			_registeredNamespaces = {},
+			_callbacks = {},
+			_getModuleNameWithNamespace = function (moduleFullName) {
+				var modulePathChain = moduleFullName.split("."),
+					nsName;
+				for (nsName in _registeredNamespaces) {
+					if (nsName === modulePathChain[0]) {
+						modulePathChain[0] = _registeredNamespaces[nsName];
+						moduleFullName = modulePathChain.join(".");
+						break;
 					}
-					return moduleFullName;
+				}
+				return moduleFullName;
+			},
+			_getModuleFilePath = function (moduleFullName, fileExt) {
+				var moduleFullNameWithNamespace = _getModuleNameWithNamespace(moduleFullName);
+				return moduleFullNameWithNamespace.replace(/\./g, "/") + "." + fileExt;
+			},
+			_loaders = {
+				css: function (stylesheetFullName) {
+					_registerModule(stylesheetFullName, "@import url(" + _getModuleFilePath(stylesheetFullName, "css") + ");");
 				},
-				_getModuleFilePath = function (moduleFullName, fileExt) {
-					var moduleFullNameWithNamespace = _getModuleNameWithNamespace(moduleFullName);
-					return moduleFullNameWithNamespace.replace(/\./g, "/") + "." + fileExt;
+				html: function (templateFullName) {
+					var iframeElem = __doc.createElement("IFRAME");
+					iframeElem.setAttribute("style", "position: absolute; left: 0px; top: 0px; width: 0px; height: 0px; visibility: hidden;");
+					iframeElem.setAttribute("src", _getModuleFilePath(templateFullName, "html"));
+					iframeElem.addEventListener("load", function () {
+						var templateString = iframeElem.contentWindow.document.body.innerHTML,
+							htmlTemplateEngine = BSS.templateEngine(templateString);
+						__head.removeChild(iframeElem);
+						_registerModule(templateFullName, htmlTemplateEngine);
+					}, false);
+					__head.appendChild(iframeElem);
 				},
-				_loaders = {
-					css: function (stylesheetFullName) {
-						_registerModule(stylesheetFullName, "@import url(" + _getModuleFilePath(stylesheetFullName, "css") + ");");
-					},
-					html: function (templateFullName) {
-						var iframeElem = _doc.createElement("IFRAME");
-						iframeElem.setAttribute("style", "position: absolute; left: 0px; top: 0px; width: 0px; height: 0px; visibility: hidden;");
-						iframeElem.setAttribute("src", _getModuleFilePath(templateFullName, "html"));
-						iframeElem.addEventListener("load", function () {
-							var templateString = iframeElem.contentWindow.document.body.innerHTML,
-								htmlTemplateEngine = BSS.templateEngine(templateString);
-							_head.removeChild(iframeElem);
-							_registerModule(templateFullName, htmlTemplateEngine);
-						}, false);
-						_head.appendChild(iframeElem);
-					},
-					js: function (moduleFullName) {
-						var scriptElem = _doc.createElement("SCRIPT");
-						scriptElem.setAttribute("type", "text/javascript");
-						scriptElem.setAttribute("charset", "utf-8");
-						scriptElem.addEventListener("load", function () {
-							_head.removeChild(scriptElem);
-						}, false);
-						scriptElem.setAttribute("src", _getModuleFilePath(moduleFullName, "js"));
-						_head.appendChild(scriptElem);
+				js: function (moduleFullName) {
+					var scriptElem = __doc.createElement("SCRIPT");
+					scriptElem.setAttribute("type", "text/javascript");
+					scriptElem.setAttribute("charset", "utf-8");
+					scriptElem.addEventListener("load", function () {
+						__head.removeChild(scriptElem);
+					}, false);
+					scriptElem.setAttribute("src", _getModuleFilePath(moduleFullName, "js"));
+					__head.appendChild(scriptElem);
+				}
+			},
+			_loadModule = function (moduleFullName, callback) {
+				var loader = _loaders.js,
+					loaderName;
+
+				for (loaderName in _loaders) {
+					if (0 === moduleFullName.indexOf(loaderName.toUpperCase() + ":")) {
+						moduleFullName = moduleFullName.substr(loaderName.length + 1);
+						loader = _loaders[loaderName];
+						break;
 					}
-				},
-				_loadModule = function (moduleFullName, callback) {
-					var loader = _loaders.js,
-						loaderName;
+				}
 
-					for (loaderName in _loaders) {
-						if (0 === moduleFullName.indexOf(loaderName.toUpperCase() + ":")) {
-							moduleFullName = moduleFullName.substr(loaderName.length + 1);
-							loader = _loaders[loaderName];
-							break;
-						}
-					}
-
-					if (!_definedModules[moduleFullName]) {
-						if (callback) {
-							_callbacks[moduleFullName] = callback;
-						}
-
-						loader(moduleFullName);
-					}
-				},
-				_loadModules = function (modulesFullNames, callback) {
-					var modulesCount = modulesFullNames && modulesFullNames.length || 0,
-						loadedModulesCount = 0,
-						callbackArgs = [],
-						getEachModuleCallback = function (index) {
-							return callback && function (module) {
-								callbackArgs[index] = module;
-								++loadedModulesCount;
-								if (loadedModulesCount === modulesCount) {
-									callback.apply(_win, callbackArgs);
-								}
-							} || null;
-						},
-						i;
-
-					for (i = 0; i < modulesCount; i++) {
-						_loadModule(modulesFullNames[i], getEachModuleCallback(i));
-					}
-
-					if (callback && 0 === modulesCount) {
-						callback.apply(_win, callbackArgs);
-					}
-				},
-				_registerModule = function (moduleFullName, module) {
-					var modulePathChain = moduleFullName.split("."),
-						ancestorsCount = modulePathChain.length - 1,
-						moduleName = modulePathChain[ancestorsCount],
-						parentObj = _bss,
-						callback = _callbacks[moduleFullName],
-						i, ancestorName;
-
-					for (i = 0; i < ancestorsCount; i++) {
-						ancestorName = modulePathChain[i];
-						if (ancestorName) {
-							if (!parentObj[ancestorName]) {
-								_win.Object.defineProperty(parentObj, ancestorName, {
-									writable: false,
-									value: {}
-								});
-							}
-							parentObj = parentObj[ancestorName];
-						}
-					}
-
-					_win.Object.defineProperty(parentObj, moduleName, {
-						writable: false,
-						value: module
-					});
-
+				if (!_definedModules[moduleFullName]) {
 					if (callback) {
-						delete _callbacks[moduleFullName];
-						callback(module);
+						_callbacks[moduleFullName] = callback;
 					}
-				};
 
-			return {
-				registerNamespace: function (name, path) {
-					_registeredNamespaces[name] = path;
-				},
-				define: function (moduleFullName, deps, moduleGetter) {
-					if (!_definedModules[moduleFullName]) {
-						_definedModules[moduleFullName] = true;
-						_loadModules(deps, function () {
-							var moduleCtor = moduleGetter.apply(_win, arguments);
-							_registerModule(moduleFullName, moduleCtor);
-						});
+					loader(moduleFullName);
+				}
+			},
+			_loadModules = function (modulesFullNames, callback) {
+				var modulesCount = modulesFullNames && modulesFullNames.length || 0,
+					loadedModulesCount = 0,
+					callbackArgs = [],
+					getEachModuleCallback = function (index) {
+						return callback && function (module) {
+							callbackArgs[index] = module;
+							++loadedModulesCount;
+							if (loadedModulesCount === modulesCount) {
+								callback.apply(__win, callbackArgs);
+							}
+						} || null;
+					},
+					i;
+
+				for (i = 0; i < modulesCount; i++) {
+					_loadModule(modulesFullNames[i], getEachModuleCallback(i));
+				}
+
+				if (callback && 0 === modulesCount) {
+					callback.apply(__win, callbackArgs);
+				}
+			},
+			_registerModule = function (moduleFullName, module) {
+				var modulePathChain = moduleFullName.split("."),
+					ancestorsCount = modulePathChain.length - 1,
+					moduleName = modulePathChain[ancestorsCount],
+					parentObj = __bss,
+					callback = _callbacks[moduleFullName],
+					i, ancestorName;
+
+				for (i = 0; i < ancestorsCount; i++) {
+					ancestorName = modulePathChain[i];
+					if (ancestorName) {
+						if (!parentObj[ancestorName]) {
+							__win.Object.defineProperty(parentObj, ancestorName, {
+								writable: false,
+								value: {}
+							});
+						}
+						parentObj = parentObj[ancestorName];
 					}
-				},
-				require: function (requiredCode, callback) {
-					var requireExecuter = ("string" === typeof requiredCode) ? _loadModule : _loadModules;
-					requireExecuter(requiredCode, callback);
+				}
+
+				__win.Object.defineProperty(parentObj, moduleName, {
+					writable: false,
+					value: module
+				});
+
+				if (callback) {
+					delete _callbacks[moduleFullName];
+					callback(module);
 				}
 			};
-		})(),
-		_bssModules_preloadFake = (function () {
-			var _argsForDeferred = [],
-				_fakeRequireAllowed = true;
 
-			return {
-				registerNamespace: function (name, path) {
-					_bssModules.registerNamespace.apply(_bssModules, arguments);
-				},
-				define: function (moduleFullName, deps, moduleGetter) {
-					_bssModules.define.apply(_bssModules, arguments);
-				},
-				require: function (requiredCode, callback) {
-					if (_fakeRequireAllowed) {
-						_argsForDeferred[_argsForDeferred.length] = [requiredCode, callback];
-					} else {
-						_bssModules.require.apply(_bssModules, arguments);
-					}
-				},
-				requireDeferred: function () {
-					var argsForDeferredCount = _argsForDeferred.length,
-						i;
-					_fakeRequireAllowed = false;
-					for (i = 0; i < argsForDeferredCount; i++) {
-						_bssModules.require.apply(_bssModules, _argsForDeferred[i]);
-					}
+		return {
+			registerNamespace: function (name, path) {
+				_registeredNamespaces[name] = path;
+			},
+			define: function (moduleFullName, deps, moduleGetter) {
+				if (!_definedModules[moduleFullName]) {
+					_definedModules[moduleFullName] = true;
+					_loadModules(deps, function () {
+						var moduleCtor = moduleGetter.apply(__win, arguments);
+						_registerModule(moduleFullName, moduleCtor);
+					});
 				}
-			};
-		})();
+			},
+			require: function (requiredCode, callback) {
+				var requireExecuter = ("string" === typeof requiredCode) ? _loadModule : _loadModules;
+				requireExecuter(requiredCode, callback);
+			}
+		};
+	})();
+	__bssModules_preloadFake = (function () {
+		var _argsForDeferred = [],
+			_fakeRequireAllowed = true;
 
-	_bssModules.registerNamespace("", _coreNamespace || "");
-	_bssModules.require(_coreModules, function () {
-		_bssModules_preloadFake.requireDeferred();
-		_win.Object.defineProperty(_bss, "modules", {
+		return {
+			registerNamespace: function (name, path) {
+				__bssModules.registerNamespace.apply(__bssModules, arguments);
+			},
+			define: function (moduleFullName, deps, moduleGetter) {
+				__bssModules.define.apply(__bssModules, arguments);
+			},
+			require: function (requiredCode, callback) {
+				if (_fakeRequireAllowed) {
+					_argsForDeferred[_argsForDeferred.length] = [requiredCode, callback];
+				} else {
+					__bssModules.require.apply(__bssModules, arguments);
+				}
+			},
+			requireDeferred: function () {
+				var argsForDeferredCount = _argsForDeferred.length,
+					i;
+				_fakeRequireAllowed = false;
+				for (i = 0; i < argsForDeferredCount; i++) {
+					__bssModules.require.apply(__bssModules, _argsForDeferred[i]);
+				}
+			},
+			dispose: function () {
+				_argsForDeferred = null;
+				_fakeRequireAllowed = null;
+				__bssModules_preloadFake = null;
+			}
+		};
+	})();
+
+	__bssModules.registerNamespace("", __coreNamespace);
+	__bssModules.require(__coreModules, function () {
+		__bssModules_preloadFake.requireDeferred();
+		__win.Object.defineProperty(__bss, "modules", {
 			writable: false,
-			value: _bssModules
+			value: __bssModules
 		});
+		__bssModules_preloadFake.dispose();
 	});
 
-	_win.Object.defineProperties(_bss, {
+	__win.Object.defineProperties(__bss, {
 		"window": {
 			writable: false,
-			value: _win
+			value: __win
 		},
 		"document": {
 			writable: false,
-			value: _doc
+			value: __doc
 		},
 		"head": {
 			writable: false,
-			value: _head
+			value: __head
 		},
 		"modules": {
 			writable: true,
-			value: _bssModules_preloadFake
+			value: __bssModules_preloadFake
 		}
 	});
 
-	_win.Object.defineProperty(_win, "BSS", {
+	__win.Object.defineProperty(__win, "BSS", {
 		writable: false,
-		value: _bss
+		value: __bss
 	});
 })(window, "Core", [
 	".routes",
