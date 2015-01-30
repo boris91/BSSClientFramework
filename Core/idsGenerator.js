3643b4f53a25f65d1b9fd68fe1ea38ffeaee371c
Boris Karbanovich <karb@scand.com>
2015-01-30 14:06:22 +0300

    BSS.idsGenerator - added internal names to implemented functions for better stack trace view during debugging.

--- Core/idsGenerator.js
+++ Core/idsGenerator.js
@@ -1,4 +1,4 @@
-BSS.modules.define(".idsGenerator", null, function () {
+BSS.modules.define(".idsGenerator", null, function BSS$modules$define_moduleGetter_idsGenerator () {
 	"use strict";
 
 	var _idLength = 32,
@@ -7,7 +7,7 @@ BSS.modules.define(".idsGenerator", null, function () {
 		CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
 		CHARS_COUNT = CHARS.length,
 		_simpleCounter = -1,
-		_generateGuid = function () {
+		_generateGuid = function BSS$modules$idsGenerator$_generateGuid () {
 			var id = "",
 				i;
 			for (i = 0; i < _idLength; i++) {
@@ -15,7 +15,7 @@ BSS.modules.define(".idsGenerator", null, function () {
 			}
 			_ids[_ids.length] = id;
 		},
-		_generatePortion = function (portionCount) {
+		_generatePortion = function BSS$modules$idsGenerator$_generatePortion (portionCount) {
 			var i;
 			portionCount = portionCount || _portionCount;
 			for (i = 0; i < portionCount; i++) {
@@ -26,22 +26,22 @@ BSS.modules.define(".idsGenerator", null, function () {
 	_generatePortion(1000);
 
 	return {
-		setIdLength: function (value) {
+		setIdLength: function BSS$modules$idsGenerator$setIdLength (value) {
 			_idLength = value;
 		},
-		setPortionCount: function (value) {
+		setPortionCount: function BSS$modules$idsGenerator$setPortionCount (value) {
 			_portionCount = value;
 		},
-		getIdForDomElement: function (prefix) {
+		getIdForDomElement: function BSS$modules$idsGenerator$getIdForDomElement (prefix) {
 			return prefix ? prefix + "_" + (++_simpleCounter) : (++_simpleCounter);
 		},
-		getId: function () {
+		getId: function BSS$modules$idsGenerator$getId () {
 			if (1 === _ids.length) {
 				_generatePortion();
 			}
 			return _ids.pop();
 		},
-		getRange: function (count) {
+		getRange: function BSS$modules$idsGenerator$getRange (count) {
 			count = ("number" === typeof count && count > 0) ? count : _portionCount;
 			if (count > _ids.length) {
 				_generatePortion(count - _ids.length + 1);
